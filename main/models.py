2648f0d0e23d0e2147d84f7e4cd4cf159b24e6bd
CNDLS <cndls_developers@georgetown.edu>
2015-09-24 17:37:17 -0400

    fix bug in Membership model __unicode__(), when group or role is undefined.

--- main/models.py
+++ main/models.py
@@ -172,13 +172,11 @@ class Membership(models.Model):
     
     def __unicode__(self):
         group_name = self.game_group.name
-        if group_name==None:
-            group_name = "Unknown Group"
         role_name = self.role.name
-        if role_name==None:
-            role_name = "Unknown Role"
-        
-        return "{}:{}".format(group_name, role_name)
+        try:
+            return "{}:{}".format(group_name, role_name)
+        except:
+            return "unknown"
         
     def games(self):
         return self.game_group.games
