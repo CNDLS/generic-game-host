c0d7a0f7b05558164412799b22634e1e2a7056f9
Yong Lee <yonglee@ycl-mba.local>
2015-09-29 19:52:26 -0400

    Changes to nav on public site, still need to add pages nav transition on scroll

--- static/js/public.js
+++ static/js/public.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
-	var header = $(".header");
-	var logo = $("#logo");
-	var mainNav = $(".main-nav");
-	var mainwrap = $(".main-wrap");
+	var header = $(".headerWrapper");
+	var spacer = $(".spacer");
+	var logo = $(".logo");
+	var navContainer = $(".navContainer");
 
 	$(window).scroll(function () {
 		var scroll = $(window).scrollTop();
 
 		if (scroll >= 30) {
-			// header.removeClass("full");
+			console.log("300")
 			header.addClass("smaller");
+			spacer.addClass("smaller");
+			logo.addClass("smaller");
+			navContainer.addClass("smaller");
 		}
+
 		else {
 			header.removeClass("smaller");
-			// header.addClass("full");
+			spacer.removeClass("smaller");
+			logo.removeClass("smaller");
+			navContainer.removeClass("smaller");
 		}
 	});
 });
